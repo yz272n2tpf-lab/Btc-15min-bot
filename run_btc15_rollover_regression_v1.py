11888b522a2d6f05f76414837515964f61c81def
yz272n2tpf-lab <yz272n2tpf@privaterelay.appleid.com>
2026-09-22 12:18:55 -0400

    Include rollover negative paths in regression suite

--- run_btc15_rollover_regression_v1.py
+++ run_btc15_rollover_regression_v1.py
@@ -5,7 +5,7 @@ MODULES=[
 "test_btc15_rollover_handoff_model_v1",
 "test_btc15_rollover_stage_selector_v1",
 "test_btc15_rollover_static_regression_gate_v1",
-"test_expose_btc15_generated_runtime_v1",\n"test_replay_btc15_rollover_live_evidence_v1",
+"test_expose_btc15_generated_runtime_v1",\n"test_replay_btc15_rollover_live_evidence_v1",\n"test_btc15_rollover_negative_paths_v1",
 ]
 if __name__=="__main__":
  suite=unittest.TestSuite()
